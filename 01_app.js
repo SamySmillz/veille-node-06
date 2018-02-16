d9ad81169476bf5e113dd7dfbd39ebe982a724b6
Samuel Celestin <mrsamuelcelestin@hotmail.com>
2018-02-16 15:04:59 -0500

    Pouvoir trier en toggle la liste des membres de maniere ascendante ou descandante

--- 01_app.js
+++ 01_app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
-const ObjectID = require('mongodb').ObjectID;
 app.use(express.static('public'));
-
+const util = require("util");
 const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID;
 const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs'); // générateur de template
@@ -44,47 +44,29 @@ app.get('/delete/:id', (req, res) => {
 app.get('/trier/:cle/:ordre', (req, res) => {
 	let cle = req.params.cle
 	let ordre = (req.params.ordre == "asc" ? 1 : -1)
-	console.log(ordre);
+
 	let cursor = db.collection('adresse').find().sort(cle,ordre).toArray(function(err, resultat) {
-		console.log(ordre);
-		ordre == "asc"? ordre="dsc": ordre="asc"
+		console.log(req.params.ordre);
+		ordre = (req.params.ordre == "asc" ? "desc" : "asc")
 		console.log(ordre);
 		res.render('gabarit.ejs', {adresses: resultat, cle, ordre})
 	})
-}) 
+})
 
 
-// app.post('/modifier', (req, res) => {
-// console.log('req.body' + req.body)
-// 	 if (req.body['_id'] != __________){ 
+app.post('/modifier', (req, res) => {
+req.body._id = ObjectID(req.body._id);
 
-// 	 console.log('sauvegarde') 
-
-// 	 var oModif = {
-// 		 "_id": ObjectID(req.body['_id']), 
-// 		 nom: req.body._____,
-// 		 prenom:req.body.______, 
-// 		 telephone:req.body._______
-// 	 }
-
-// 	 var util = require("util");
-// 	 console.log('util = ' + util.inspect(oModif));
+ 	 console.log('util = ' + util.inspect(req.body));
 	 
-// 	}
-// 	else{
-// 		 console.log('insert')
-// 		 console.log(req.body)
-// 		 var oModif = {
-// 		 nom: req.body.______,
-// 		 prenom:req.body.______, 
-// 		 telephone:req.body._______
-// 	}
-// }
-//  db.collection('adresse').save(oModif, (err, result) => {
-// 	 if (err) return console.log(err)
-// 	 console.log('sauvegarder dans la BD')
-// 	 res.redirect('/list')
-//  })
+
+  db.collection('adresse').save(req.body, (err, result) => {
+ 	 if (err) return console.log(err)
+	 console.log('sauvegarder dans la BD')
+	 res.redirect('/list')
+ })
+
+})
 ////////////////////////////connexion a MangoDB et au serveur node.js
 
 
